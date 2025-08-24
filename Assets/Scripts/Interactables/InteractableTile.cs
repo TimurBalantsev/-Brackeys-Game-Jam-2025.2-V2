1020a6bf70e1faf3bfdf730f7965152cb0cf303c
Creeperkatze <178587183+Creeperkatze@users.noreply.github.com>
2025-08-24 21:06:34 +0200

    tilemap stuff

---
+++ Assets/Scripts/Interactables/InteractableTile.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+[CreateAssetMenu(menuName = "Tiles/Interactable Tile")]
+public class InteractableTile : TileBase
+{
+    // Test
+
+    public string interactionName;
+    public Sprite sprite;
+    public GameObject prefab;
+
+    public override void GetTileData(Vector3Int position, ITilemap tilemap, ref TileData tileData)
+    {
+        tileData.sprite = sprite;
+        tileData.color = Color.white;
+        tileData.colliderType = Tile.ColliderType.Sprite;
+    }
+}
