b3de4c164444305765abcc2b5f65c334440a1f19
tbalantsev <tbalantsev@gmail.com>
2025-08-27 12:37:01 -0400

    added house

--- Assets/Scripts/Interactables/Container.cs
+++ Assets/Scripts/Interactables/Container.cs
@@ -4,17 +4,21 @@ using UnityEngine;
 public class Container : MonoBehaviour, Interactable
 {
     [SerializeField] private Inventory inventory;
-    [SerializeField] private SpriteRenderer spriteRenderer;
+    // [SerializeField] private SpriteRenderer spriteRenderer;
+    [SerializeField] private SpriteRenderer[] spriteRenderers;
+    [SerializeField] private Color defaultColor = Color.white;
     [SerializeField] private Color highlightColor = Color.yellow;
     [SerializeField] private WeightedLootTableSO lootTable;
     [SerializeField] private int maxItemsSpawned;
     [SerializeField] private int minItemsSpawned;
     [SerializeField] private AudioClipSO openingSound;
-    private Color defaultColor;
     
     private void Start()
     {
-        defaultColor = spriteRenderer.color;
+        if (spriteRenderers.Length == 0)
+        {
+            Debug.LogError($"{this} has no sprite rendrer");
+        }
         if (lootTable == null) return;
         int amountItems = Random.Range(minItemsSpawned, maxItemsSpawned);
         for (int i = 0; i < amountItems; i++)
@@ -31,7 +35,12 @@ public class Container : MonoBehaviour, Interactable
 
     public void Select(Player player, bool isSelected)
     {
-        spriteRenderer.color = isSelected ? highlightColor : defaultColor;
+        foreach (SpriteRenderer spriteRenderer in spriteRenderers)
+        {
+            spriteRenderer.color = isSelected ? highlightColor : defaultColor;
+
+        }
+        // spriteRenderer.color = isSelected ? highlightColor : defaultColor;
     }
 
     public Transform GetTransform()
